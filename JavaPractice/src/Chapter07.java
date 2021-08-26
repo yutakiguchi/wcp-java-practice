c625a46e1863c77cbfacf20157d1bd2512576823
yutakiguchi <y--k080816@docomo.ne.jp>
2021-08-26 23:10:16 +0900

    繰り返し

--- JavaPractice/src/Chapter07.java
+++ JavaPractice/src/Chapter07.java
@@ -11,5 +11,23 @@ public class Chapter07 {
 		}else {
 			System.out.println("とても遠いです");
 		}
+		
+		String color = "yellow";
+		switch(color) {
+			case "red":
+				System.out.println("赤信号");
+				break;
+			case "yellow":
+				System.out.println("黄色信号");
+				break;
+			case "blue":
+				System.out.println("青信号");
+				break;
+			default:
+				System.out.println("信号の色はありません");
+			 
+			 
+		}
 	}
+	
 }
