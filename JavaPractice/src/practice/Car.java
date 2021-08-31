72ac1d56ae1930c9091f12020e930d30d5d772af
yutakiguchi <y--k080816@docomo.ne.jp>
2021-08-31 17:49:16 +0900

    chapter9

---
+++ JavaPractice/src/practice/Car.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class Car {
+	
+	private double fuelCost;
+	private double fuelAmount;
+	
+	public Car(double fuelCost, double fuelAmount) {
+		this.fuelCost = fuelCost;
+		this.fuelAmount = fuelAmount;
+	}
+	
+	public double move(int km) {
+		System.out.println(km+"km走ります");
+		return this.fuelAmount-=(km/fuelCost);
+	}
+	
+	public double getFuelAmount() {
+		return this.fuelAmount;
+	}
+	
+	
+}
